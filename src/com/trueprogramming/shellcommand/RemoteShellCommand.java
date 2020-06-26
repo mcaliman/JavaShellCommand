fc7732f790c4579a7527ae9b99feace038e4d1ea
Massimo Caliman <mcaliman@gmail.com>
2020-06-26 08:45:03 +0200

    Add skeleton for remote shell

---
+++ src/com/trueprogramming/shellcommand/RemoteShellCommand.java
@@ -0,0 +1,24 @@
+package com.trueprogramming.shellcommand;
+
+/**
+ *
+ * @author Massim Caliman
+ */
+public class RemoteShellCommand implements ShellCommand {
+
+    @Override
+    public void execute() throws ShellCommandException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getExitValue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StringBuilder getOutput() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
